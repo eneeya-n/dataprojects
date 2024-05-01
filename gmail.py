d7b46bda54cd958142ba0eb07e8349765d7918fe
eneeya-n <91284294+eneeya-n@users.noreply.github.com>
2024-05-01 08:05:38 +0530

    Add files via upload

---
+++ gmail.py
@@ -0,0 +1,33 @@
+import oauth
+from email.mime.text import MIMEText
+import base64
+import math
+import random
+
+def create_message(sender, to, subject, message_text):
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+def send_message(sender,to,subject,message_text,user_id='me'):
+  msg = create_message(sender,to,subject,message_text)
+  try:
+    service = oauth.get_g_service()
+    message = (service.users().messages().send(userId=user_id, body=msg)
+               .execute())
+    return message
+  except ValueError as e:
+    print('An error occurred: %s' % e)
+
+
+def send_otp(user_mail):
+    digits = "0123456789"
+    OTP = ""
+    for i in range (6):
+        OTP += digits[math.floor(random.random()*10)]
+    otp = OTP + " is your OTP"
+    message = otp
+    send_message("harijo560@gmail.com",user_mail,"Your OTP for Verification",message,user_id='me')
+    return OTP
